ab5a9242edaca9ccce777d2a0f150f6657376c8a
Yamanoborisu <65376569+Yamanoborisu@users.noreply.github.com>
2020-08-18 18:15:54 +0900

    Update isObjEmpty.js
    + alternative way

--- isObjEmpty.js
+++ isObjEmpty.js
@@ -2,3 +2,15 @@ function isEmpty (obj) {
 let propArray = Object.keys(obj);
 return (propArray.length == 0) ? true : false;
 }
+
+
+
+or 
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if body is began to execute then prop exist
+    return false;
+  }
+  return true;
+}
